141f4b69ba059538133a23be3ee67cd842881c1e
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-01 17:31:51 +0530

    changed the structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import BookSearch from './Pages/BookSearch'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import BookSearch from './Pages/BookSearch';
+import './App.css';
 
-//function App 
-export default function App() {
-      return (
-        <div className="app">
-            {/* Router is used for page routing */}
-            <Route exact path="/" component={ Home }/> 
-            <Route path="/search" component={ BookSearch }/>
-        </div>
-      )
-}
+const App = () => (
+  <Router>
+    <div className="app">
+      <Route exact path="/" component={Home} />
+      <Route path="/search" component={BookSearch} />
+    </div>
+  </Router>
+);
+
+export default App;
