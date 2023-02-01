3f85129caf7093fc2e0252ccf16df4f2b6348a42
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-01 17:29:49 +0530

    fix: converted to function, useState and useEffect

--- src/Pages/BookSearch.js
+++ src/Pages/BookSearch.js
@@ -1,112 +1,91 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Book from '../Section/Book'
 import * as BooksAPI from '../BooksAPI'
 
-//class BookSearch
-//working should be
-//to search and return match book list
-//if not found show - not found
-//works properly with multiple words, noo book thumbnail or author
+const BookSearch = ({ location }) => {
+const [books, setBooks] = useState([])
+const [searchResult, setSearchResult] = useState([])
+const [searchError, setSearchError] = useState(false)
 
-export default class BookSearch extends Component {
+useEffect(() => {
+    setBooks(location.state.booksFromHome)
+}, [location.state.booksFromHome])
 
-    state = {
-        books:[],
-        searchResult:[],
-        searchError:false
-    }
-
-    componentDidMount(){
-        this.setState({
-            books: this.props.location.state.booksFromHome})
-    }
-
-    searching = event => {
-        const searchInput = event.target.value
-        
-        if(searchInput) {
-            BooksAPI.search(searchInput).then((resultBooks)=>{
-                if(!resultBooks || resultBooks.hasOwnProperty('error')){
-                    this.setState({searchResult: [], searchError: true })
-                } else {
-                    this.setState({searchResult: resultBooks, searchError:false})
-                    this.syncBookShelf()
-                }
-            })
-        } else {
-            this.setState({searchResult: [] })
-        }
-    }
-
-    syncBookShelf = () => {
-        const books= this.state.books
-        const searchResult = this.state.searchResult
-        if(searchResult.length > 0) {
-                books.forEach((book) => {
-                    searchResult.forEach((searchResultBook) =>{
-                        if(book.id === searchResultBook.id) {
-                            searchResultBook.shelf = book.shelf
-                        }
-                    })
-                })
-        }
-        this.setState({searchResult: searchResult})
-    }
-
-    changeShelf = (book,shelf) => {
-        BooksAPI.update(book,shelf).then((result) => {
-            book.shelf = shelf
-            var updatedBooks = this.state.books.filter((resultBook) =>resultBook.id !== book.id)
-            updatedBooks.push(book)
-            this.setState({books: updatedBooks})
+const searching = event => {
+    const searchInput = event.target.value
+    
+    if(searchInput) {
+        BooksAPI.search(searchInput).then((resultBooks)=>{
+            if(!resultBooks || resultBooks.hasOwnProperty('error')){
+                setSearchResult([])
+                setSearchError(true)
+            } else {
+                setSearchResult(resultBooks)
+                setSearchError(false)
+                syncBookShelf()
+            }
         })
-
+    } else {
+        setSearchResult([])
     }
+}
 
-   render() {
-       const searchResult = this.state.searchResult
-       const searchError = this.state.searchError
-       return(
-        <div className="search-books">
-            <div className="search-books-bar">
+const syncBookShelf = () => {
+    books.forEach((book) => {
+        searchResult.forEach((searchResultBook) =>{
+            if(book.id === searchResultBook.id) {
+                searchResultBook.shelf = book.shelf
+            }
+        })
+    })
+    setSearchResult(searchResult)
+}
+
+const changeShelf = (book,shelf) => {
+    BooksAPI.update(book,shelf).then((result) => {
+        book.shelf = shelf
+        var updatedBooks = books.filter((resultBook) =>resultBook.id !== book.id)
+        updatedBooks.push(book)
+        setBooks(updatedBooks)
+    })
+
+}
+
+return (
+    <div className="search-books">
+        <div className="search-books-bar">
             <Link to="/" className="close-search">Close</Link>
-                    <div className="search-books-input-wrapper">
-                    {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                    */}
-                    <input
-                         type="text" onChange={this.searching} placeholder="Search by title or author"/>
-                    </div>
-            </div>
-            <div className="search-books-results">
-                {searchResult.length>0 && (
-                <div>
-                    <div>
-                        <h3> {searchResult.length} books found!</h3>
-                    </div>
-                <ol className="books-grid">
-                    {searchResult.map((book) =>(
-                        <Book
-                            key={book.id}
-                            book={book}
-                            changeShelf={this.changeShelf}
-                        />
-                    ))}
-                </ol>
-                </div>
-                )}
-                {searchError && (
-                    <div>
-                        <h3>No book found. Please try again !</h3>
-                    </div>
-                )}
+            <div className="search-books-input-wrapper">
+                <input
+                    type="text" onChange={searching} placeholder="Search by title or author"/>
             </div>
         </div>
-       )
-   }
+        <div className="search-books-results">
+            {searchResult.length>0 && (
+            <div>
+                <div>
+                    <h3> {searchResult.length} books found!</h3>
+                </div>
+            <ol className="books-grid">
+                {searchResult.map((book) =>(
+                    <Book
+                        key={book.id}
+                        book={book}
+                        changeShelf={changeShelf}
+                    />
+                ))}
+            </ol>
+            </div>
+            )}
+            {searchError && (
+                <div>
+                    <h3>No book found. Please try again !</h3>
+                </div>
+            )}
+        </div>
+    </div>
+)
 }
+
+export default BookSearch;
