e1b15cec60d9f1f481d3640d8c050b2638f0191b
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-01 17:33:47 +0530

    fix:Warning ReactDOM.render is no longer supported

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-// BrowserRouter is imported 
-ReactDOM.render(
-<BrowserRouter>
-        {/* func App is wrapped into BrowserRouter */}
-        <App />
-</BrowserRouter>, document.getElementById('root'))
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
