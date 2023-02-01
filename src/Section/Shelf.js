4e07d252ba6a9dc74f37a7f17acd6bf34a195b38
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-01 17:30:06 +0530

    fix: coverted to function

--- src/Section/Shelf.js
+++ src/Section/Shelf.js
@@ -1,66 +1,60 @@
-import React, { Component } from 'react'
-import * as BooksAPI from '../BooksAPI'
-import ShelfSection from './ShelfSection'
-import AddBook from './AddBook'
+import React, { useState, useEffect } from "react";
+import * as BooksAPI from "../BooksAPI";
+import ShelfSection from "./ShelfSection";
+import AddBook from "./AddBook";
 
-//class Shelf
-export default class Shelf extends Component {
+const Shelf = () => {
+  const [books, setBooks] = useState([]);
 
-    state = {
-        books: []
-    }
+  useEffect(() => {
+    BooksAPI.getAll().then((books) => {
+      setBooks(books);
+    });
+  }, []);
 
-    componentDidMount() 
-    {
-        BooksAPI.getAll().then((books) => {
-            this.setState({
-                books: books
-            })
-        })
-    }
+  const changeShelf = (book, newShelf) => {
+    BooksAPI.update(book, newShelf).then(() => {
+      book.shelf = newShelf;
+      setBooks((prevBooks) =>
+        prevBooks.filter((b) => b.id !== book.id).concat([book])
+      );
+    });
+  };
 
-    changeShelf = (book, newShelf) => 
-    {
-        BooksAPI.update(book,newShelf).then((result) =>{
-            console.log('Update response', result)
-            book.shelf = newShelf
-            var updatedBooks = this.state.books.filter((resultBook) =>resultBook.id !== book.id)
-            updatedBooks.push(book)
-            this.setState({books: updatedBooks})
-        })
-    }
+  //Book shelf titles
+  const compartments = [
+    { type: "currentlyReading", title: "In Progress" },
+    { type: "wantToRead", title: "Next Up" },
+    { type: "read", title: "Completed" },
+  ];
 
-    render() {
+  return (
+    <div>
+      <div className="list-books-content">
+        {books.length > 0 && (
+          <div>
+            {compartments.map((compartment, index) => {
+              const compartmentBooks = books.filter(
+                (book) => book.shelf === compartment.type
+              );
+              return (
+                <div className="bookshelf" key={index}>
+                  <h2 className="bookshelf-title">{compartment.title}</h2>
+                  <ShelfSection
+                    key={index}
+                    books={compartmentBooks}
+                    compartmentsList={compartments}
+                    changeShelf={changeShelf}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <AddBook currentBooks={books} />
+    </div>
+  );
+};
 
-        //Book shelf titles
-        const compartments = [
-            {type: 'currentlyReading', title: 'In Progress'},
-            {type: 'wantToRead', title: 'Next Up'},
-            {type: 'read', title: 'Completed'}
-        ]
-        return(
-            <div>
-            <div className='list-books-content'>
-                { this.state.books.length > 0 &&
-                    <div>
-                        { compartments.map( (compartment,index) => {
-                            const compartmentBooks =this.state.books.filter( (book) =>
-                            book.shelf===compartment.type
-                         )
-                            return(
-                                <div className="bookshelf" key={index}>
-                                    <h2 className="bookshelf-title">{compartment.title}</h2>
-                                        <ShelfSection key={index} books={compartmentBooks} compartmentsList={compartments} changeShelf={this.changeShelf}/>
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </div>
-            <AddBook
-                currentBooks={this.state.books}
-            />
-            </div>
-        )
-    }
-}
+export default Shelf;
