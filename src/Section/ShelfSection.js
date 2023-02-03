5a414c51e510bd1da1f82d3e2f91a9275fbc7de9
prosenjeetshil <prosenjeetshil@gmail.com>
2023-02-03 10:09:58 +0530

    fix:  Warning: Failed prop type

--- src/Section/ShelfSection.js
+++ src/Section/ShelfSection.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import Book from './Book'
-import PropTypes from 'prop-types'
+import React from "react";
+import Book from "./Book";
+import PropTypes from "prop-types";
 
 //function ShelfSection
-export default function ShelfSection(props) 
-{
-    const { compartmentIndex, books, changeShelf } = props
-    return (
-        <div>
-            <div className="bookshelf-books" key={compartmentIndex}>
-                <ol className="books-grid">
-                    {books.map((book) => (
-                        <Book
-                            key={book.id}
-                            book={book}
-                            changeShelf={changeShelf} />
-                    ))}
-                </ol>
-            </div>
-        </div>
-    )
+export default function ShelfSection(props) {
+  const { compartmentIndex, books, changeShelf } = props;
+  return (
+    <div>
+      <div className="bookshelf-books" key={compartmentIndex}>
+        <ol className="books-grid">
+          {books.map((book) => (
+            <Book key={book.id} book={book} changeShelf={changeShelf} />
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 // PropTypes are used to make sure the datatype receive is valid
 ShelfSection.propTypes = {
-    books: PropTypes.array.isRequired,
-    compartmentIndex: PropTypes.number.isRequired,
-    changeShelf: PropTypes.func.isRequired
+  books: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired,
 };
